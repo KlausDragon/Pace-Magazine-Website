3729d70f2868b58424d20834054cf1b9c33f7acd
Ali A <90362620+KlausDragon@users.noreply.github.com>
2022-10-18 15:54:51 -0700

    Add files via upload

---
+++ ContactUs.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    $('.submit').click (function(event) {
+        event.preventDefault()
+        console.log('clicked button')
+    
+        var email = $('.email').val()
+        var name = $('.name').val()
+        var message = $('.message').val()
+        var statusElm = $('.status')
+        statusElm.empty()
+    
+        if (email.length > 5 && email.includes('@') && email.includes('.')){
+            statusElm.append('<div>Email is valid</div>')
+        } else {
+            statusElm.append('<div>Email is not valid</div>')
+        }
+    
+        if (name.length >= 2) {
+            statusElm.append('<div>Name is valid</div>')
+        } else {
+            statusElm.append('<div>Name is not valid</div>')
+        }
+    
+        if (message.length >= 5) {
+            statusElm.append('<div>Message is valid</div>')
+        } else {
+            statusElm.append('<div>Message is not valid</div>')
+        }
+    })
+    })
